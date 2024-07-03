461c7ce91c9d07c0a4830b9187b9ba8060c896de
Luidy <luidywertuly@gmail.com>
2024-07-03 16:20:45 -0300

    Versão final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>bem vindo</h1>
       </header>
+
+      <body>
+        <h1>teste</h1>
+      </body>
+
+      <footer>
+        <h1>Adeus</h1>
+      </footer>
+      
     </div>
   );
 }
